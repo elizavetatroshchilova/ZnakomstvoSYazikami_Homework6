bb86928e719a08ba7c3ef061cb2b20b758224f8a
Elizabeth <qazwsx050816@gmail.com>
2023-02-26 15:56:49 +0300

    Add 6 homework

---
+++ Zadanie41/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите элементы через пробел и нажмите Enter: ");
+
+int CalculateCount = EnterNumbers();
+
+Console.WriteLine($"из всех введенных чисел {CalculateCount} больше 0");
+
+
+int EnterNumbers()
+{
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+return count;
+}
+ 
+
---
+++ Zadanie43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+int IntInput(string intName)
+{
+    int num;
+
+    Console.Write($"Введите число {intName}: ");
+    while (!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Введено недопустимое значение");
+
+    }
+    return num;
+}
+
+int k1 = IntInput("k1");
+int k2 = IntInput("k2");
+int b1 = IntInput("b1");
+int b2 = IntInput("b2");
+
+double deltaK = k1 - k2;
+double deltaB = b1 - b2;
+double x = (-1) * deltaB / deltaK;
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых - ({x};{y})");
